fa211fd175546b4b4c5ef2c53a9e155ee6f6624c
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-01-24 15:34:05 +0000

    fix create data sims

--- create_data.py
+++ create_data.py
@@ -7,20 +7,20 @@ np.random.seed(0)
 
 if __name__ == '__main__':
 
-    train_sims = 40000
+    train_sims = 18000
     test_sims = 2000
     for i in tqdm(range(train_sims), desc='Training Set'):
-        N = np.random.randint(1, 100)
+        N = np.random.randint(1, 200)
         rot_rate = 1
-        sim = Simulation(N=N, v0=1, L_box=1.0, t_max=0.1, couple_radius=0.05, rot_rate=rot_rate, seed=i, timesteps=2)
+        sim = Simulation(N=N, v0=0.1, L_box=1.0, delta_t=0.1, couple_radius=0.1, rot_rate=rot_rate, seed=i, timesteps=2)
         sim.solve_dynamics(method='RK45')
         _, loc = sim.get_solution()
         np.save(f'./data/simulation_train_{i}.npy', loc)
     
     for i in tqdm(range(test_sims), desc='Test Set'):
-        N = np.random.randint(100, 120)
+        N = np.random.randint(180, 250)
         rot_rate = 1
-        sim = Simulation(N=N, v0=1, L_box=1.0, t_max=0.1, couple_radius=0.05, rot_rate=rot_rate, seed=i, timesteps=2)
+        sim = Simulation(N=N, v0=0.1, L_box=1.0, delta_t=0.1, couple_radius=0.1, rot_rate=rot_rate, seed=i, timesteps=2)
         sim.solve_dynamics(method='RK45')
         _, loc = sim.get_solution()
         np.save(f'./data/simulation_test_{i}.npy', loc)
--- launcher.py
+++ launcher.py
@@ -6,6 +6,6 @@ if __name__ == '__main__':
 
     N = 1000
     rot_rate = 1
-    sim = Simulation(N=N, v0=0.1, L_box=1.0, delta_t=0.1, couple_radius=0.05, rot_rate=rot_rate, timesteps=100)
+    sim = Simulation(N=N, v0=0.1, L_box=1.0, delta_t=0.1, couple_radius=0.1, rot_rate=rot_rate, timesteps=100)
     sim.solve_dynamics(method='RK45')
     sim.create_animation(filename=None)
