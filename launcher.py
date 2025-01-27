17399d66de57e4da7ef04b45eaf01a92cd3d29b5
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-01-27 11:02:38 +0000

    test

--- launcher.py
+++ launcher.py
@@ -9,3 +9,5 @@ if __name__ == '__main__':
     sim = Simulation(N=N, v0=0.1, L_box=1.0, delta_t=0.1, couple_radius=0.1, rot_rate=rot_rate, timesteps=100)
     sim.solve_dynamics(method='RK45')
     sim.create_animation(filename=None)
+    _, loc = sim.get_solution()
+    np.save(f'./data/test_1000.npy', loc)
