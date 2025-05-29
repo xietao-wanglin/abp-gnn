7b42139dfd8bdc0fa4de24a5e8fd9ebd34f3edd9
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-05-29 14:32:16 +0100

    fix scaling script

--- scalings/scaling_model.py
+++ scalings/scaling_model.py
@@ -4,8 +4,8 @@ import torch
 
 import time
 
-from src.continuous.models import GNN
-from src.continuous.utils import compute_graph, apply_periodic_boundary
+from src.discrete.models import GNN
+from src.discrete.utils import compute_graph, apply_periodic_boundary
 
 device = 'cpu'
 dtype = torch.float
