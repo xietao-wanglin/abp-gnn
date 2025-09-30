cc5cce1b3820ecd643c15239ec5eca3d85900121
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-09-30 14:54:07 +0100

    clean codebase

--- datasets/abps/create_data.py
+++ datasets/abps/create_data.py
@@ -12,11 +12,13 @@ import json
 
 def generate_state(n, box_length=1.0):
     rot_rate = np.ones(n) * 0
-    v0 = np.ones(n) * 0
+    v0 = np.ones(n) * 0.1
     rot_couple = np.zeros(n)
-    sigma = 0.04
-    epsilon = 0.001
-    law_power = 1
+    sigma = 0.01
+    epsilon = 0.1
+    law_power = 6
+    noise_t = 0
+    noise_d = 4
     boundary_type = (1, 1, 1)
 
     initial_state = np.random.random(3 * n)
@@ -33,6 +35,8 @@ def generate_state(n, box_length=1.0):
         sigma,
         epsilon,
         law_power,
+        noise_t,
+        noise_d,
         boundary_type,
         initial_state,
     )
@@ -41,15 +45,20 @@ def generate_state(n, box_length=1.0):
 def compute_stats(script_dir):
     train_glob = sorted(glob(f"{script_dir}/data/simulation_train_*"))
     all_list = np.array([])
+    all_list_theta = np.array([])
     for data in train_glob:
         data_arr = np.load(data)
         bcs = apply_periodic_boundary(torch.tensor(data_arr[::2])).numpy()
         pos_diff = data_arr[1::2] - bcs
         df = np.sqrt(pos_diff[:, 0] ** 2 + pos_diff[:, 1] ** 2).reshape(-1)
+        df_theta = np.sqrt(pos_diff[:, 2] ** 2).reshape(-1)
         all_list = np.hstack([all_list, df])
+        all_list_theta = np.hstack([all_list, df_theta])
     df_describe = pd.DataFrame(all_list)
+    df_describe_theta = pd.DataFrame(all_list_theta)
     mean, std = df_describe.mean()[0], df_describe.std()[0]
-    return mean, std
+    angular_mean, angular_std = df_describe_theta.mean()[0], df_describe_theta.std()[0]
+    return mean, std, angular_mean, angular_std
 
 
 if __name__ == "__main__":
@@ -76,6 +85,8 @@ if __name__ == "__main__":
             sigma,
             epsilon,
             law_power,
+            noise_t,
+            noise_d,
             boundary_type,
             initial_state,
         ) = generate_state(n=n)
@@ -83,17 +94,19 @@ if __name__ == "__main__":
             v0=v0,
             rot_couple=rot_couple,
             rot_rate=rot_rate,
-            timesteps=100,
+            timesteps=1000,
             sigma=sigma,
             epsilon=epsilon,
             law_power=law_power,
             attr_strength=0,
             seed=i,
             initial_state=initial_state,
+            noise_t=noise_t,
+            noise_d=noise_d,
             boundary_type=boundary_type,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs()
+        _times, loc = sim.get_solution_abs(every=10)
         np.save(f"{script_dir}/{data_folder}/simulation_train_{i}.npy", loc[10:])
 
     for i in tqdm(range(test_init, test_sims + test_init), desc="Test Set"):
@@ -106,6 +119,8 @@ if __name__ == "__main__":
             sigma,
             epsilon,
             law_power,
+            noise_t,
+            noise_d,
             boundary_type,
             initial_state,
         ) = generate_state(n=n)
@@ -113,17 +128,19 @@ if __name__ == "__main__":
             v0=v0,
             rot_couple=rot_couple,
             rot_rate=rot_rate,
-            timesteps=100,
+            timesteps=1000,
             sigma=sigma,
             epsilon=epsilon,
             law_power=law_power,
             attr_strength=0,
             seed=98743 * i + 4500,
             initial_state=initial_state,
+            noise_t=noise_t,
+            noise_d=noise_d,
             boundary_type=boundary_type,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs()
+        _times, loc = sim.get_solution_abs(every=10)
         np.save(f"{script_dir}/{data_folder}/simulation_test_{i}.npy", loc[10:])
 
     for i in tqdm(range(long_test_sims), desc="Long Test Set"):
@@ -136,6 +153,8 @@ if __name__ == "__main__":
             sigma,
             epsilon,
             law_power,
+            noise_t,
+            noise_d,
             boundary_type,
             initial_state,
         ) = generate_state(n=n)
@@ -150,17 +169,19 @@ if __name__ == "__main__":
             timesteps=400,
             seed=983 * i + 2000,
             initial_state=initial_state,
+            noise_t=noise_t,
+            noise_d=noise_d,
             boundary_type=boundary_type,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs()
+        _times, loc = sim.get_solution_abs(every=10)
         np.save(f"{script_dir}/{data_folder}/simulation_long_test_{i}.npy", loc[10:])
 
     vel_mean, vel_std = compute_stats(script_dir)
     stats = {
         "vel_mean": vel_mean,
         "vel_std": vel_std,
-        "angular_mean": 0.1,
+        "angular_mean": 0,
         "angular_std": 0,
     }
     metadata_path = os.path.join(script_dir, "metadata.json")
