8619ac7fa4c56489529adbea88f370b2c1bbd08c
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-10-06 13:25:28 +0100

    change login to be on slurm file

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ def main():
     args = parser.parse_args()
 
     base_cfg = OmegaConf.load(args.config)
-    wandb.login()
     wandb.init(
         project="ABP_GNN",
         name=base_cfg.wandb.name,
