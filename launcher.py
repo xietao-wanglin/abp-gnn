5a3f23fd7e6859340d7be5f6a2c66bf7ef072b92
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-10-26 16:03:52 +0000

    abp_boundaries

--- launcher.py
+++ launcher.py
@@ -1,4 +1,4 @@
-from src.simulation import WCA2
+from src.simulation import WCA
 
 import numpy as np
 
@@ -14,40 +14,6 @@ def generate_state(n, box_length=1.0):
     return initial_state
 
 
-def generate_state_with_grid_boundary(
-    lc, n_boundary=400, sigma=0.04, central_fraction=0.25
-):
-    n_side = int(np.sqrt(n_boundary))
-    box_length = n_side * lc
-
-    x = np.linspace(0, box_length - lc, n_side) + lc / 2
-    y = np.linspace(0, box_length - lc, n_side) + lc / 2
-    X, Y = np.meshgrid(x, y)
-    X, Y = X.flatten()[:n_boundary], Y.flatten()[:n_boundary]
-
-    central_width = box_length * central_fraction
-    offset = (box_length - central_width) / 2
-
-    while True:
-        active_x = np.random.rand() * central_width + offset
-        active_y = np.random.rand() * central_width + offset
-        d = np.sqrt((X - active_x) ** 2 + (Y - active_y) ** 2)
-        if np.all(d > 1 * sigma):
-            break
-    active_theta = np.random.rand() * 2 * np.pi
-
-    all_x = np.concatenate([X, [active_x]])
-    all_y = np.concatenate([Y, [active_y]])
-    all_theta = np.concatenate([np.zeros(n_boundary), [active_theta]])
-
-    n_total = n_boundary + 1
-    particle_type = np.zeros(n_total, dtype=int)
-    particle_type[-1] = 1
-
-    initial_state = np.vstack([all_x, all_y, all_theta])
-    return particle_type, initial_state, box_length
-
-
 def load_from_file(filepath):
     pos = np.load(filepath)
     return pos[0]
@@ -55,22 +21,16 @@ def load_from_file(filepath):
 
 if __name__ == "__main__":
     np.random.seed(0)
-    lc = 0.115
-    particle_type, initial_state, box_length = generate_state_with_grid_boundary(lc)
-    # initial_state = generate_state(40)
-    sim = WCA2(
-        v0=0.04 * 3,
+    initial_state = generate_state(30)
+    sim = WCA(
+        v0=0.1,
         initial_state=initial_state,
-        rot_rate=1,
+        rot_rate=0.0,
         sigma=0.04,
+        epsilon=0.1,
         timesteps=160,
         delta_t=0.1,
-        rot_couple=0,
-        diffusion_r=0.0,
-        diffusion_t=0.0,
-        particle_type=particle_type,
-        box_length=box_length,
+        box_length=0.4
     )
     sim.solve_dynamics(method="RK45", debug=True)
-    _times, loc = sim.get_solution()
-    np.save(f"./analysis/data/lc-{lc}.npy", loc)
+    sim.create_animation()
