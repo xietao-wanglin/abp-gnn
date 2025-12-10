e66f5f24d01e4eb89225ce56da6a4088db4d1282
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2025-12-10 17:23:11 +0000

    add wca simulations

--- fast_launcher.py
+++ fast_launcher.py
@@ -1,8 +1,10 @@
-from src.fast_simulation import Toy
+from src.fast_simulation import WCA
 import jax
 import jax.numpy as jnp
 import numpy as np
+
 jax.config.update("jax_enable_x64", True)
+
 
 def generate_state(key, n, box_length=1.0):
     key, subkey = jax.random.split(key)
@@ -14,17 +16,23 @@ def generate_state(key, n, box_length=1.0):
 
     initial_state = initial_state.reshape(n, 3).T
 
-    return initial_state, key
+    particle_type = jnp.ones(n)
+
+    return particle_type, initial_state, key
 
 
 if __name__ == "__main__":
     key = jax.random.PRNGKey(0)
-    initial_state, _key = generate_state(key, n=30)
-    sim = Toy(
+    particle_type, initial_state, _key = generate_state(key, n=30)
+    sim = WCA(
         initial_state=initial_state,
         v0=0.1,
         rot_rate=0.0,
-        epsilon=0.025,
+        epsilon=0.1,
+        sigma=0.04,
+        couple_radius=0.1,
+        couple_strength=0.1,
+        particle_type=particle_type,
     )
-    out = sim.solve_dynamics(t_end=20, dt=1e-4, save_dt=0.1, debug=True)
-    np.save("test/toy.npy", out)
+    out = sim.solve_dynamics(t_end=40, dt=1e-4, save_dt=0.1, debug=True)
+    np.save("test/abp.npy", out)
