5e4a9c0f9681ecb4e785a260b98dcf9f3be3d6bb
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2026-02-14 19:13:52 +0000

    chevron sims

--- chevron_det.py
+++ chevron_det.py
@@ -1,27 +1,30 @@
-from src.simulation import WCA
-
+from src.fast_simulation import TorqueWCA
+import jax
+import jax.numpy as jnp
 import numpy as np
+import argparse
 
+jax.config.update("jax_enable_x64", True)
 
-
-def generate_state_chevron(n, n_boundary=50, box_length=1.0, chevron_angle=90, arm_length=0.4):
+def generate_state_chevron(n, box_length, n_boundary=30, chevron_angle=60, arm_length=0.4):
     n_free = n
 
     half_angle_rad = np.deg2rad(chevron_angle / 2)
-
+    
     x_free = np.random.random(n_free) * box_length
     y_free = np.random.random(n_free) * box_length
     theta_free = np.random.random(n_free) * 2 * np.pi
 
     center = np.array([box_length / 2, box_length / 2])
     arm_points = n_boundary // 2
-
     s = np.linspace(0, arm_length * box_length, arm_points)
-    x_left = center[0] - s * np.cos(half_angle_rad)
-    y_left = center[1] - s * np.sin(half_angle_rad)
+    dx = s * np.sin(half_angle_rad)
+    dy = s * np.cos(half_angle_rad)
+    x_left = center[0] - dx
+    y_left = center[1] - dy 
 
-    x_right = center[0] + s * np.cos(half_angle_rad)
-    y_right = center[1] - s * np.sin(half_angle_rad)
+    x_right = center[0] + dx
+    y_right = center[1] - dy 
 
     x_bound = np.concatenate([x_left, x_right])
     y_bound = np.concatenate([y_left, y_right])
@@ -32,34 +35,58 @@ def generate_state_chevron(n, n_boundary=50, box_length=1.0, chevron_angle=90, a
     theta_all = np.concatenate([theta_bound, theta_free])
 
     initial_state = np.vstack([x_all, y_all, theta_all])
-    particle_type = np.ones(n+n_boundary, dtype=int)
+    particle_type = np.ones(n + n_boundary, dtype=int)
     particle_type[: len(x_bound)] = 0
+
+    initial_state = jnp.asarray(initial_state)
+    particle_type = jnp.asarray(particle_type)
 
     return initial_state, particle_type
 
+
 if __name__ == "__main__":
-    n = 1
-    sigma = 0.02
-    box_length = 1.0
-    initial_state, particle_type = generate_state_chevron(n=n, n_boundary=20, box_length=box_length)
-    sim = WCA(
+    np.random.seed(1)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("index", help="index")
+    args = parser.parse_args()
+
+    start_phi = 0
+    end_phi = 160
+    n_phi = 41 
+
+    phis = [start_phi + i * (end_phi - start_phi) / (n_phi - 1) for i in range(n_phi)]
+    index = int(args.index)
+    phi = phis[index]
+
+    n = 200
+    save_dt = 1
+    sigma = 0.04
+    box_length = 1
+    initial_state, particle_type= generate_state_chevron(
+        n=n, chevron_angle=phi, n_boundary=50, box_length=box_length, arm_length=0.3,
+    )
+    sim = TorqueWCA(
         initial_state=initial_state,
         v0=0.1,
-        diffusion_r=0.001,
-        diffusion_t=0.0,
         rot_rate=0.0,
+        epsilon=0.1,
         sigma=sigma,
-        epsilon=0.01,
-        timesteps=5000,
         couple_radius=0.0,
-        rot_couple=0.0,
-        delta_t=0.1,
+        couple_strength=0.0,
+        gamma=1,
+        particle_type=particle_type,
         box_length=box_length,
-        record_every=10,
-        start_record=0,
-        particle_type=particle_type
     )
-    sim.solve_dynamics(method="RK45", debug=True)
-    _times, loc = sim.get_solution()
-    sim.create_animation()
-    #np.save(f"initial_conditions/n_{n}_{i}.npy", loc[-2])
+    out = sim.solve_dynamics(
+        t_end=1000, dt=1e-12, save_dt=save_dt, debug=True, use_controller=True
+    )
+    res = np.array(out.block_until_ready())
+    np.savez(
+        "sim_out",
+        predictions=res,
+        box_length=box_length,
+        initial_state=np.array(initial_state),
+        angle=phi,
+        dt=save_dt,
+    )
