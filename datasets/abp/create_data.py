2d160cd68332548ed53963d420fc1ed508ab703c
Xietao Wang Lin <Xietao.Wang-Lin@warwick.ac.uk>
2026-03-17 16:32:22 +0000

    abp

--- datasets/abp/create_data.py
+++ datasets/abp/create_data.py
@@ -10,57 +10,53 @@ import os
 import json
 
 
-def generate_state(n, box_length=1.0):
-    delta_t = 0.001
-    rot_rate = np.ones(n) * 0
-    rot_couple = np.ones(n) * 0
+def generate_state(n):
+    rot_rate = np.zeros(n)
+    rot_couple = np.zeros(n)
     sigma = 0.04
-    diffusion_r = 0.001
-    diffusion_t = 0.001
+    box_length = 0.4
+    epsilon = 0.1
 
     initial_state = np.random.random(3 * n)
     initial_state[2::3] *= 2 * np.pi
-    initial_state[0::3] = initial_state[0::3] * box_length
-    initial_state[1::3] = initial_state[1::3] * box_length
-
+    initial_state[0::3] *= box_length
+    initial_state[1::3] *= box_length
     initial_state = initial_state.reshape(n, 3).T
 
     return (
-        delta_t,
         rot_rate,
         rot_couple,
         sigma,
-        diffusion_r,
-        diffusion_t,
+        epsilon,
+        box_length,
         initial_state,
     )
 
 
 def compute_stats(script_dir):
-    train_glob = sorted(glob(f"{script_dir}/data/simulation_train_*"))
-    all_list = np.array([])
-    all_list_theta = np.array([])
-    for data in train_glob:
-        data_arr = np.load(data)
-        bcs = apply_periodic_boundary(torch.tensor(data_arr[::2])).numpy()
+    box_length = 0.4
+    sim_glob = sorted(glob(f"{script_dir}/data/simulation_train_*"))
+    all_list = []
+    for idx, data in enumerate(sim_glob):
+        data_arr = np.load(data)[:2]
+        bcs = apply_periodic_boundary(
+            torch.tensor(data_arr[::2]), dims=[box_length, box_length, 2 * torch.pi]
+        ).numpy()
         pos_diff = data_arr[1::2] - bcs
-        df = np.sqrt(pos_diff[:, 0] ** 2 + pos_diff[:, 1] ** 2).reshape(-1)
-        df_theta = np.sqrt(pos_diff[:, 2] ** 2).reshape(-1)
-        all_list = np.hstack([all_list, df])
-        all_list_theta = np.hstack([all_list, df_theta])
+        df = np.sqrt(pos_diff[:, 0] ** 2 + pos_diff[:, 1] ** 2)
+        all_list.append(df.reshape(-1))
+    all_list = np.hstack(all_list)
     df_describe = pd.DataFrame(all_list)
-    df_describe_theta = pd.DataFrame(all_list_theta)
     mean, std = df_describe.mean()[0], df_describe.std()[0]
-    angular_mean, angular_std = df_describe_theta.mean()[0], df_describe_theta.std()[0]
-    return mean, std, angular_mean, angular_std
+    return mean, std
 
 
 if __name__ == "__main__":
-    train_sims = 1000
+    train_sims = 0
     train_init = 0
-    test_sims = 200
+    test_sims = 0
     test_init = 0
-    long_test_sims = 4
+    long_test_sims = 0
     data_folder = "data"
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -71,91 +67,82 @@ if __name__ == "__main__":
 
     for i in tqdm(range(train_init, train_sims + train_init), desc="Training Set"):
         np.random.seed(i)
-        n = np.random.randint(30, 50)
+        n = np.random.randint(10, 70)
         (
-            delta_t,
             rot_rate,
             rot_couple,
             sigma,
-            diffusion_r,
-            diffusion_t,
+            epsilon,
+            box_length,
             initial_state,
         ) = generate_state(n=n)
         sim = WCA(
-            delta_t=delta_t,
             rot_couple=rot_couple,
             rot_rate=rot_rate,
-            diffusion_r=diffusion_r,
-            diffusion_t=diffusion_t,
-            timesteps=10000,
+            timesteps=2,
             sigma=sigma,
-            seed=i,
+            epsilon=epsilon,
+            box_length=box_length,
             initial_state=initial_state,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs(every=100)
-        np.save(f"{script_dir}/{data_folder}/simulation_train_{i}.npy", loc[10:])
+        _times, loc = sim.get_solution_abs()
+        np.save(f"{script_dir}/{data_folder}/simulation_train_{i}.npy", loc)
 
     for i in tqdm(range(test_init, test_sims + test_init), desc="Test Set"):
         np.random.seed(98743 * i + 4500)
-        n = np.random.randint(30, 50)
+        n = np.random.randint(10, 70)
         (
-            delta_t,
             rot_rate,
             rot_couple,
             sigma,
-            diffusion_r,
-            diffusion_t,
+            epsilon,
+            box_length,
             initial_state,
         ) = generate_state(n=n)
         sim = WCA(
-            delta_t=delta_t,
             rot_couple=rot_couple,
             rot_rate=rot_rate,
-            diffusion_r=diffusion_r,
-            diffusion_t=diffusion_t,
-            timesteps=10000,
+            timesteps=22,
             sigma=sigma,
-            seed=98743 * i + 4500,
+            epsilon=epsilon,
+            box_length=box_length,
             initial_state=initial_state,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs(every=100)
-        np.save(f"{script_dir}/{data_folder}/simulation_test_{i}.npy", loc[10:])
+        _times, loc = sim.get_solution_abs()
+        np.save(f"{script_dir}/{data_folder}/simulation_test_{i}.npy", loc)
 
     for i in tqdm(range(long_test_sims), desc="Long Test Set"):
         np.random.seed(983 * i + 2000)
-        n = np.random.randint(45, 70)
+        n = np.random.randint(70, 80)
         (
-            delta_t,
             rot_rate,
             rot_couple,
             sigma,
-            diffusion_r,
-            diffusion_t,
+            epsilon,
+            box_length,
             initial_state,
         ) = generate_state(n=n)
         sim = WCA(
-            delta_t=delta_t,
             rot_couple=rot_couple,
             rot_rate=rot_rate,
-            diffusion_r=diffusion_r,
-            diffusion_t=diffusion_t,
             sigma=sigma,
-            timesteps=40000,
-            seed=983 * i + 2000,
+            epsilon=epsilon,
+            box_length=box_length,
+            timesteps=400,
             initial_state=initial_state,
         )
         sim.solve_dynamics(method="RK45")
-        _times, loc = sim.get_solution_abs(every=100)
-        np.save(f"{script_dir}/{data_folder}/simulation_long_test_{i}.npy", loc[10:])
+        _times, loc = sim.get_solution_abs()
+        np.save(f"{script_dir}/{data_folder}/simulation_long_test_{i}.npy", loc)
 
-    vel_mean, vel_std, angular_mean, angular_std = compute_stats(script_dir)
+    vel_mean, vel_std = compute_stats(script_dir)
     stats = {
         "vel_mean": vel_mean,
         "vel_std": vel_std,
-        "angular_mean": angular_mean,
-        "angular_std": angular_std,
+        "angular_mean": 0,
+        "angular_std": 0,
     }
     metadata_path = os.path.join(script_dir, "metadata.json")
     with open(metadata_path, "w") as f:
